99b52b5390cfafd10e7d876e8ec5fbdeb5c5b413
sarath <sarathfmm18@gmail.com>
2022-02-15 20:13:18 +0530

    admob

--- src/components/Advert.tsx
+++ src/components/Advert.tsx
@@ -1,31 +1,50 @@
-import { ADVERT_CAP_TIME, FB_PLACEMENT_AD, ADMOB_UNIT_ID } from "../constants";
+import { ADVERT_CAP_TIME,ADMOB_UNIT_ID } from "../constants";
+import mobileAds from 'react-native-google-mobile-ads';
+import { InterstitialAd, TestIds, AdEventType } from 'react-native-google-mobile-ads';
 
 var shouldShowAd = true;
 
-export default function loadAd() {
-	console.log(shouldShowAd, "Should the ad be shown?");
-	// if(shouldShowAd){
-	//     try{
-	//         interstitialAd.show();
-	//     } catch(e) {
-	//         console.log(e)
-	//     }
-	//     preloadAd()
-	//     shouldShowAd=false;
-	//     setTimeout(function(){
-	//         shouldShowAd = true
-	//         },ADVERT_CAP_TIME/2)
-	// }
+mobileAds()
+  .initialize()
+  .then((adapterStatuses) => {
+    // Initialization complete!
+    console.log("Initialization complete!")
+	
+  });
+
+  const interstitial = InterstitialAd.createForAdRequest(ADMOB_UNIT_ID, {
+	//requestNonPersonalizedAdsOnly: true,
+	//keywords: ['fashion', 'clothing'],
+  });
+
+  const eventListener = interstitial.onAdEvent(type => {
+	if (type === AdEventType.LOADED) {
+		console.log("AD LOADED")
+	}
+  });
+
+  export default function loadAd(){
+    console.log(shouldShowAd, "Should the ad be shown?")
+    if(shouldShowAd){
+        try{
+            interstitial.show();
+        } catch(e) {
+            console.log(e)
+        }
+        preloadAd()
+        shouldShowAd=false;
+        setTimeout(function(){
+            shouldShowAd = true
+            },ADVERT_CAP_TIME/2)
+    }
 }
 
-export function preloadAd() {
-	// try{
-	//     interstitialAd.load();
-	// } catch(e) {
-	//     console.log(e)
-	// }
-	/*
-    InterstitialAdManager.preloadAd(FB_PLACEMENT_AD)
-    .then((didClick) => {console.log(didClick,"PreLoad")})
-    .catch((error) => {console.log(error,"PreLoad")});*/
+export function preloadAd(){
+    try{
+        interstitial.load();
+    } catch(e) {
+        console.log(e)
+    }
+    
+   
 }
